605872b985a521e9def6f9662a29842f41357ecd
[Sean Hagen] <sean.r.hagen@ndus.edu>
2020-07-18 18:46:53 -0500

    Added timer, time duration setting so program executes automatically

--- SecurityWebcam/Form1.Designer.cs
+++ SecurityWebcam/Form1.Designer.cs
@@ -34,78 +34,141 @@
             this.label4 = new System.Windows.Forms.Label();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(33, 60);
+            this.button1.Location = new System.Drawing.Point(31, 57);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(102, 23);
+            this.button1.Size = new System.Drawing.Size(135, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Take Picture";
+            this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(191, 66);
+            this.label1.Location = new System.Drawing.Point(224, 59);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(32, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "Null";
-            
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(345, 60);
+            this.label4.Location = new System.Drawing.Point(380, 20);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(82, 17);
             this.label4.TabIndex = 6;
             this.label4.Text = "Magin Error";
-          
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(610, 55);
+            this.numericUpDown1.Location = new System.Drawing.Point(383, 57);
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.Size = new System.Drawing.Size(79, 22);
             this.numericUpDown1.TabIndex = 7;
             this.numericUpDown1.Value = new decimal(new int[] {
-            5,
+            10,
             0,
             0,
             0});
-           
             // 
             // imageBox1
             // 
-            this.imageBox1.Location = new System.Drawing.Point(45, 169);
+            this.imageBox1.Location = new System.Drawing.Point(31, 139);
             this.imageBox1.Name = "imageBox1";
-            this.imageBox1.Size = new System.Drawing.Size(293, 299);
+            this.imageBox1.Size = new System.Drawing.Size(353, 354);
             this.imageBox1.TabIndex = 2;
             this.imageBox1.TabStop = false;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(224, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Same Picture?";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
             // imageBox2
             // 
-            this.imageBox2.Location = new System.Drawing.Point(439, 169);
+            this.imageBox2.Location = new System.Drawing.Point(417, 139);
             this.imageBox2.Name = "imageBox2";
-            this.imageBox2.Size = new System.Drawing.Size(291, 299);
-            this.imageBox2.TabIndex = 2;
+            this.imageBox2.Size = new System.Drawing.Size(353, 354);
+            this.imageBox2.TabIndex = 9;
             this.imageBox2.TabStop = false;
-           
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(589, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Time Duration";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(484, 60);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Percent";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(592, 59);
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(79, 22);
+            this.numericUpDown2.TabIndex = 12;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(710, 60);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "seconds";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 524);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.imageBox1);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.label4);
@@ -116,6 +179,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -128,7 +192,12 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Label label2;
         private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- SecurityWebcam/Form1.cs
+++ SecurityWebcam/Form1.cs
@@ -17,50 +17,86 @@ namespace ImageHashingApp
 {
     public partial class Form1 : Form
     {
-        private VideoCapture cap;
-        Queue<Emgu.CV.Mat> queue;
+        private static VideoCapture cap;
+        private static Queue<Emgu.CV.Mat> queue;
+        private static Timer t;
+        private bool exitFlag = true;
         public Form1()
         {
             InitializeComponent();
             cap = new VideoCapture();
             queue = new Queue<Emgu.CV.Mat>(2);
-
+            t = new Timer();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //Yes, I am using .text as a boolean variable. Fight me.
+            if (button1.Text.Equals("Start"))
+            {
+                button1.Text = "Stop";
+                exitFlag = true;
+                t.Tick += imageCompare;
+                t.Interval = (int)(numericUpDown2.Value * 1000);
+                t.Enabled = true;
+                t.Start();
+            }
+            else
+            {
+                t.Stop();
+                button1.Text = "Start";
+                // Processes all the events in the queue.
+                Application.DoEvents();
+            }
+        }
 
+        private void imageCompare(Object source, EventArgs e)
+        {
             queue.Enqueue(cap.QueryFrame());
             imageBox1.Image = queue.Peek().ToImage<Emgu.CV.Structure.Bgr, Byte>(false).Resize(imageBox1.Width, imageBox1.Height, Emgu.CV.CvEnum.Inter.Lanczos4);
             imageBox2.Image = queue.Last().ToImage<Emgu.CV.Structure.Bgr, Byte>(false).Resize(imageBox1.Width, imageBox1.Height, Emgu.CV.CvEnum.Inter.Lanczos4);
-            //string two = "C:\\Users\\seanr\\source\\repos\\ImageHashingApp\\Dog.jpg";
-            this.label1.Text = imageCompare().ToString();
-        }
-
-        private bool imageCompare()
-        {
-            if (queue.Count > 1)    
+            if (queue.Count > 1)
             {
                 Emgu.CV.Mat imageHashOne = new Mat(), imageHashTwo = new Mat();
                 Emgu.CV.Mat a = queue.Dequeue();
                 Emgu.CV.Mat b = queue.Peek();
 
-                AverageHash t = new Emgu.CV.ImgHash.AverageHash();
+                //This is apparently the best hash method
+                MarrHildrethHash t = new Emgu.CV.ImgHash.MarrHildrethHash();
                 t.Compute(a, imageHashOne);
                 t.Compute(b, imageHashTwo);
                 double percentageImageSimilarity = t.Compare(imageHashOne, imageHashTwo);
                 double er = 100 - percentageImageSimilarity;
                 if (percentageImageSimilarity < (int)numericUpDown1.Value)
                 {
-                    return true;
+                    label1.Text = "True";
                 }
                 else
                 {
-                    return false;
+                    label1.Text = "False";
                 }
             }
-            else { return false; }
+            else { label1.Text = "False"; }
         }
-    
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void imageBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
